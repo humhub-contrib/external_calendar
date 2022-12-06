b424cc2f1abec340ef671817d73fa23a590954f2
Marc Farré <23310825+funkycram@users.noreply.github.com>
2022-12-06 17:07:00 +0100

    Update model_config.php

--- messages/fr/model_config.php
+++ messages/fr/model_config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'Post new calendar on stream' => '',
-    'Post new entries on stream' => '',
+    'Post new calendar on stream' => 'Publier un nouveau calendrier dans la vue d\'ensemble',
+    'Post new entries on stream' => 'Publier de nouvelles entrées dans la vue d\'ensemble',
 ];
