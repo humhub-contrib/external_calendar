7b493c8e574dd2d338db35da7106968d772a10be
Yuriy Bakhtin <yurybakh@gmail.com>
2023-11-01 13:45:44 +0100

    Fix tests

--- tests/codeception/unit/ImportPerformanceTest.php
+++ tests/codeception/unit/ImportPerformanceTest.php
@@ -13,6 +13,7 @@ use humhub\modules\external_calendar\models\ExternalCalendarEntry;
 use humhub\modules\external_calendar\models\ExternalCalendarEntryQuery;
 use DateTime;
 use humhub\modules\space\models\Space;
+use SebastianBergmann\Timer\Duration;
 use SebastianBergmann\Timer\Timer;
 
 /**
@@ -23,29 +24,43 @@ use SebastianBergmann\Timer\Timer;
  */
 class ImportPerformanceTest extends ExternalCalendarTest
 {
+    private ?Timer $timer = null;
+
+    public function _before()
+    {
+        $this->timer = new Timer;
+        $this->timer->start();
+    }
 
     public function testExpandRecurrence1OneYear()
     {
-        $timer = new Timer;
-        $timer->start();
         /** @var $events ExternalCalendarEntry[] * */
         $events = ExternalCalendarEntryQuery::findForFilter(
             DateTime::createFromFormat('!Ymd', '20190801', new \DateTimeZone('Europe/Berlin')),
             DateTime::createFromFormat('!Ymd', '20200801', new \DateTimeZone('Europe/Berlin')),
             Space::findOne(1));
-        print_r('Expand '.$timer->stop().' ('.count($events).")\n");
+        print_r('Expand '.$this->getDuration().' ('.count($events).")\n");
     }
 
     public function testExpandRecurrence2OneYear()
     {
-        $timer = new Timer;
-        $timer->start();
         /** @var $events ExternalCalendarEntry[] * */
         $events = ExternalCalendarEntryQuery::findForFilter(
             DateTime::createFromFormat('!Ymd', '20220801', new \DateTimeZone('Europe/Berlin')),
             DateTime::createFromFormat('!Ymd', '20230801', new \DateTimeZone('Europe/Berlin')),
             Space::findOne(1));
-        print_r('Expand '.$timer->stop().' ('.count($events).")\n");
+        print_r('Expand '.$this->getDuration().' ('.count($events).")\n");
+    }
+
+    private function getDuration()
+    {
+        $duration = $this->timer->stop();
+
+        if ($duration instanceof Duration) {
+            return $duration->asString();
+        }
+
+        return $duration;
     }
 
 }
