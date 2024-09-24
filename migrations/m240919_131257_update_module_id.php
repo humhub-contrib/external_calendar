a122c9c427bcc4868f9ddb536a73463478f44b78
Yuriy Bakhtin <yurybakh@gmail.com>
2024-09-24 16:10:41 +0200

    Fix module ID in other tables

--- migrations/m240919_131257_update_module_id.php
+++ migrations/m240919_131257_update_module_id.php
@@ -2,6 +2,9 @@
 
 use humhub\components\Migration;
 use humhub\models\ModuleEnabled;
+use humhub\models\Setting;
+use humhub\modules\activity\models\Activity;
+use humhub\modules\content\models\ContentContainerSetting;
 
 /**
  * Class m240919_131257_update_module_id
@@ -13,10 +16,17 @@ class m240919_131257_update_module_id extends Migration
      */
     public function safeUp()
     {
-        $moduleEnabled = ModuleEnabled::findOne(['module_id' => 'external_calendar']);
+        $oldId = 'external_calendar';
+        $newId = 'external-calendar';
+
+        $moduleEnabled = ModuleEnabled::findOne(['module_id' => $oldId]);
         if ($moduleEnabled) {
-            $moduleEnabled->module_id = 'external-calendar';
+            $moduleEnabled->module_id = $newId;
             $moduleEnabled->save();
+
+            Activity::updateAll(['module' => $newId], ['module' => $oldId]);
+            Setting::updateAll(['module_id' => $newId], ['module_id' => $oldId]);
+            ContentContainerSetting::updateAll(['module_id' => $newId], ['module_id' => $oldId]);
         }
     }
 
